923dc3e4ba82d1b8e02740f723723bdba9f1003c
Rasoarilala Juliette <juliette.rasoarilala@gmail.com>
2021-04-06 21:23:29 +0200

    "Create and start project" + "Create components" + "Create routes"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import {NoteComponent} from './note/note.component';
+import {NotesComponent} from './notes/notes.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'home', component: HomeComponent},
+  {path: 'notes', component: NotesComponent},
+  {path: 'note/:id', component: NoteComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
